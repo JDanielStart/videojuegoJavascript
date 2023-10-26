438c312b55e0d61e49d03df896075dfdac0de06a
JDaniel <daniel@JDStart.onmicrosoft.com>
2023-10-26 20:56:10 +0100

    Esqueleto de movimiento

--- game.js
+++ game.js
@@ -1,38 +1,47 @@
+//Variables globales fijas de HTML
 const canvas = document.querySelector("#game");
-const game = canvas.getContext("2d");
+const btnUp = document.querySelector("#up");
+const btnLeft = document.querySelector("#left");
+const btnRight = document.querySelector("#right");
+const btnDown = document.querySelector("#down");
 
+//Variables globales fijas de javascript
+const game = canvas.getContext("2d");
+const map = maps[0];
+const mapRows = map.trim().split("\n");
+const mapRowCols = mapRows.map(row => row.trim().split(""));
+const playerPosition = {
+    x: undefined,
+    y: undefined
+}
+
+//Variables globales cambiantes
 let canvasSize = 0;
 let elementSize = 0;
 
+
+//Adición de los eventos
 window.addEventListener("load", setCanvasSize);
 window.addEventListener("resize", setCanvasSize);
+window.addEventListener("keydown", moveByKeys);
+
+btnUp.addEventListener("click", moveUp);
+btnLeft.addEventListener("click", moveLeft);
+btnRight.addEventListener("click", moveRight);
+btnDown.addEventListener("click", moveDown);
 
 function startGame() {
     game.font = elementSize + "px Verdana";
     game.textAlign = "center"; // Establece la alineación en el centro
 
-    const map = maps[1];
-    const mapRows = map.trim().split("\n");
-    const mapRowCols = mapRows.map(row => row.trim().split(""));
-
-    // Centro del canvas
-    const centerX = canvasSize / 2;
-    const centerY = canvasSize / 2;
-
-    for (let i = 1; i <= 10; i++) {
-        for (let j = 1; j <= 10; j++) {
-            const emoji = emojis[mapRowCols[i - 1][j - 1]];
-            
-            // Desplazamiento en X e Y para centrar el texto en la celda
-            const xOffset = elementSize / 2;
-            const yOffset = elementSize / 2;
-            
-            const x = centerX + (i - 5) * elementSize - xOffset; // Centra horizontalmente
-            const y = centerY + (j - 5) * elementSize - yOffset; // Centra verticalmente
-
-            game.fillText(emoji, x, y);
-        }
-    }
+    mapRowCols.forEach((row, rowI) => {
+        row.forEach((col, colI) => {
+            const emoji = emojis[col];
+            const x = elementSize * (colI + 0.55); // Centra horizontalmente
+            const y = elementSize * (rowI + 0.85); // Centra verticalmente
+            game.fillText(emoji, x, y)
+        });
+    });
 }
 
 function setCanvasSize() {
@@ -43,17 +52,46 @@ function setCanvasSize() {
         canvasSize = window.innerHeight * 0.8;
     }
 
-    // Centro del canvas
-    const centerX = canvasSize / 2;
-    const centerY = canvasSize / 2;
-
     canvas.setAttribute("width", canvasSize);
     canvas.setAttribute("height", canvasSize);
 
-    elementSize = (canvasSize / 11);
+    elementSize = (canvasSize / 10);
 
     startGame();
 
+}
+
+function moveByKeys(event) {
+
+    if (event.key == "ArrowUp" || event.key == "w" || event.key == "W") {
+        console.log("arriba");
+    }
+    else if (event.key == "ArrowLeft" || event.key == "a" || event.key == "A") {
+        console.log("izquierda");
+    }
+    else if (event.key == "ArrowRight" || event.key == "d" || event.key == "D") {
+        console.log("derecha");
+    }
+    else if (event.key == "ArrowDown" || event.key == "s" || event.key == "w") {
+        console.log("abajo");
+    }
+
+}
+
+function moveUp() {
+    console.log("Arriba");
+}
+
+function moveLeft() {
+    console.log("Izqueirda");
+}
+
+function moveRight() {
+    console.log("derecha");
+}
+
+function moveDown() {
+    console.log("Abajo");
 }
 
 
