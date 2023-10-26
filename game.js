d43e39bdd20b32d939983387d69a272523f2db51
JDaniel <daniel@JDStart.onmicrosoft.com>
2023-10-27 00:09:26 +0100

    Se limita al jugador y es responsive

--- game.js
+++ game.js
@@ -19,41 +19,19 @@ const playerPosition = {
 let canvasSize = 0;
 let elementSize = 0;
 
-
-//Adición de los eventos
-window.addEventListener("load", setCanvasSize);
-window.addEventListener("resize", setCanvasSize);
+//Adición de los eventos de ventana
+window.addEventListener("load", startGame);
+window.addEventListener("resize", startGame);
 window.addEventListener("keydown", moveByKeys);
 
+//Adición de los eventos de html
 btnUp.addEventListener("click", moveUp);
 btnLeft.addEventListener("click", moveLeft);
 btnRight.addEventListener("click", moveRight);
 btnDown.addEventListener("click", moveDown);
 
+//Renderiza el juego
 function startGame() {
-    game.font = elementSize + "px Verdana";
-    game.textAlign = "center"; // Establece la alineación en el centro
-
-    mapRowCols.forEach((row, rowI) => {
-        row.forEach((col, colI) => {
-            const emoji = emojis[col];
-            const x = elementSize * (colI + 0.55); // Centra horizontalmente
-            const y = elementSize * (rowI + 0.85); // Centra verticalmente
-
-            game.fillText(emoji, x, y);
-
-            if (col == "O") {
-                if (!playerPosition.x && !playerPosition.y) {
-                    playerPosition.x = x;
-                    playerPosition.y = y;
-                }
-            }
-        });
-    });
-    movePlayer();
-}
-
-function setCanvasSize() {
     if (window.innerHeight > window.innerWidth) {
         canvasSize = window.innerWidth * 0.8;
     }
@@ -63,17 +41,43 @@ function setCanvasSize() {
 
     canvas.setAttribute("width", canvasSize);
     canvas.setAttribute("height", canvasSize);
-
     elementSize = (canvasSize / 10);
+    game.font = elementSize + "px Verdana";
+    game.textAlign = "center"; // Establece la alineación en el centro
 
-    startGame();
+    mapRowCols.forEach((row, rowI) => {
+        row.forEach((col, colI) => {
+            const emoji = emojis[col];
 
+            game.fillText(emoji, renderElementX(colI), renderElementY(rowI));
+
+            if (col == "O") {
+                if (playerPosition.x === undefined && playerPosition.y === undefined) {
+                    playerPosition.x = colI;
+                    playerPosition.y = rowI;
+                }
+            }
+        });
+    });
+    movePlayer();
 }
 
+//Calculamos la posición x de un elemento
+function renderElementX(x) {
+    return elementSize * (x + 0.55); // Centra horizontalmente
+}
+
+//Calculamos la posición y de un elemento
+function renderElementY(y) {
+    return elementSize * (y + 0.85); // Centra verticalmente
+}
+
+//Renderizamos la nueva posición del jugador
 function movePlayer() {
-    game.fillText(emojis["PLAYER"], playerPosition.x , playerPosition.y);
+    game.fillText(emojis["PLAYER"], renderElementX(playerPosition.x), renderElementY(playerPosition.y));
 }
 
+//Comprobamos si se pulsa los botones indicados para mover el jugador
 function moveByKeys(event) {
 
     if (event.key == "ArrowUp" || event.key == "w" || event.key == "W") {
@@ -92,41 +96,41 @@ function moveByKeys(event) {
 }
 
 function moveUp() {
-    if ((playerPosition.y - elementSize) < 0) {
-        console.log("Fuera");
+    if ((renderElementY(playerPosition.y) - elementSize) < 0) {
+        console.log("Reproducir sonido")
     }
     else {
-        playerPosition.y -= elementSize;
-        setCanvasSize();
+        playerPosition.y--;
+        startGame();
     }
 }
 
 function moveLeft() {
-    if ((playerPosition.x - elementSize) < 0) {
-        console.log("Fuera");
+    if ((renderElementX(playerPosition.x) - elementSize) < 0) {
+        console.log("Reproducir sonido")
     }
     else {
-        playerPosition.x -= elementSize;
-        setCanvasSize();
+        playerPosition.x--; 
+        startGame();
     }
 }
 
 function moveRight() {
-    if ((playerPosition.x + elementSize) > canvasSize) {
-        console.log("Fuera");
+    if ((renderElementX(playerPosition.x) + elementSize) > canvasSize) {
+        console.log("Reproducir sonido")
     }
     else {
-        playerPosition.x += elementSize;
-        setCanvasSize();
+        playerPosition.x++;
+        startGame();
     }
 }
 
 function moveDown() {
-    if ((playerPosition.y + elementSize) > canvasSize) {
-        console.log("Fuera");
+    if ((renderElementY(playerPosition.y) + elementSize) > canvasSize) {
+        console.log("Reproducir sonido")
     }
     else {
-        playerPosition.y += elementSize;
-        setCanvasSize();
+        playerPosition.y++;
+        startGame();
     }
 }
